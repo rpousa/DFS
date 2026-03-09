64e7b6b6113cdc6a566cec9e99f8f411224a3a98
vagrant <vagrant@comnetsemu>
2026-03-09 13:53:28 +0000

    ext_dn fix

--- docker_compose/activate_exec.sh
+++ docker_compose/activate_exec.sh
@@ -2,4 +2,4 @@ chmod +x setup.sh start.sh stop.sh
 chmod +x flexric_volumes/ric_watchdog.sh flexric_volumes/topology.py flexric_volumes/xapp_functs.py flexric_volumes/xapp_test.py
 chmod +x ue_volumes/ue_entrypoint.sh 
 chmod +x configs/mysql/mysql-healthcheck.sh
-chmod +x configs/ext_dn/trf_gen_entrypoint.sh configs/ext_dn_1/trf_gen_entrypoint.sh
+chmod +x configs/ext_dn/trfgen_entrypoint.sh configs/ext_dn_1/trfgen_entrypoint.sh
